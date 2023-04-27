b8ee20768a0da218505fcaf4f939e358deac9af3
Tsaplin Sergey <74894518+talatari@users.noreply.github.com>
2023-04-27 10:32:13 +0300

    Fix Joystick.

--- Assets/Scripts/Joystick/Joystick.cs
+++ Assets/Scripts/Joystick/Joystick.cs
@@ -37,8 +37,8 @@ namespace SpaceShooter
                 PositionJoyStick = PositionJoyStick.normalized;
             }
 
-            float offsetX = sizeDeltaX / 2 - _imageJoyStick.rectTransform.sizeDelta.x / 2;
-            float offsetY = sizeDeltaY / 2 - _imageJoyStick.rectTransform.sizeDelta.y / 2;
+            float offsetX = (sizeDeltaX / 2) - _imageJoyStick.rectTransform.sizeDelta.x / 2;
+            float offsetY = (sizeDeltaY / 2) - _imageJoyStick.rectTransform.sizeDelta.y / 2;
 
             _imageJoyStick.rectTransform.anchoredPosition = new Vector2(
                 PositionJoyStick.x * offsetX,
